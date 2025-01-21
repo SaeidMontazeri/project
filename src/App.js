ba7d496162b4ba9924deeb952d79adefb4069b35
Saeid <saeidmontazeri@outlook.com>
2025-01-21 17:16:11 +0330

    Refactor Card component to use functional component and state for price adjustment

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ function App() {
   const cars = [
     {
       name: "Audi",
-      price: "68,225 $",
+      price: 68,
       src: "https://platform.cstatic-images.com/large/in/v2/26614b17-8a14-51ca-a177-286e9977310e/7d766581-2af6-40c4-a226-3de07adfcc64/ooGGqfpvMRu5JUJTCl2Vd-Re-b4.jpg",
     },
     {
       name: "Ford",
-      price: "33,000 $",
+      price: 33,
       src: "https://platform.cstatic-images.com/large/in/v2/f6403c94-4a83-57ef-820e-1e2ecd10d807/dd531449-3cb7-47a6-b4a2-e34f43c2590f/9GBXJWbBtWR1VLq5vXxBlCI1x4A.jpg",
     },
     {
       name: "Tesla",
-      price: "50,000 $",
+      price: 50,
       src: "https://platform.cstatic-images.com/large/in/v2/7076ebed-5ff7-5143-a0ca-032abc87b1cc/15c959d0-e872-4977-b3b2-51c11f1e9138/niZexgKr1KDPawkJQQ75bFTgBLo.jpg",
     },
   ];
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,52 +1,47 @@
 import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-
-// function Card({src, firstname, lastname}) {
-//   return (
-//     <div class="card col-4" style={{ width: "400px" }}>
-//       <img class="card-img-top" src={src} height={500} alt="Card image" />
-//       <div class="card-body">
-//         <h4 class="card-title">{firstname}</h4>
-//         <p class="card-text">{lastname}</p>
-//         <a href="#" class="btn btn-primary"></a>
-//       </div>
-//     </div>
-//   );
-// }
-
-class Card extends React.Component {
-  constructor(props) {
-    super(props);
+import { useState } from "react";
+function Card(props) {
+  const [price, setPrice] = useState(props.price);
+  function Add() {
+    setPrice(price + 1);
   }
-  render() {
-    return (
-      <div class="card col-4" style={{ width: "27rem" }}>
-        <img
-          src={this.props.src}
-          class="card-img-top"
-          alt={this.props.name}
-          height={300}
-        />
-        <div class="card-body">
-          <h5 class="card-title" style={{ textAlign: "center", width: "100%" }}>
-            {this.props.name}
-          </h5>
-          <p class="card-text" style={{ textAlign: "center", width: "100%" }}>
-            {this.props.price}
-          </p>
+  function Mines() {
+    setPrice(price - 1);
+  }
+  return (
+    <div class="card col-4" style={{ width: "27rem" }}>
+      <img src={props.src} class="card-img-top" alt={props.name} height={300} />
+      <div class="card-body">
+        <h5 class="card-title" style={{ textAlign: "center", width: "100%" }}>
+          {props.name}
+        </h5>
+        <p class="card-text" style={{ textAlign: "center", width: "100%" }}>
+          {price},000 $
+        </p>
+        <div className="row">
+          <div className="col">
           <button
-            onClick={() => {
-              window.location.href = this.props.src;
-            }}
+            onClick={Add}
             className="btn btn-primary"
             style={{ textAlign: "center", width: "100%" }}
           >
-            Click me
+            +
           </button>
+          </div>
+          <div className="col">
+          <button
+            onClick={Mines}
+            className="btn btn-primary"
+            style={{ textAlign: "center", width: "100%" }}
+          >
+            -
+          </button>
+          </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default Card;
